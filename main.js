952ec4a300b09d8167fe405066c7dab155b12ce2
Rajdeep Chandra <rajdeepchandra@Rajdeeps-MacBook-Air.local>
2018-03-27 10:18:10 +0530

    first

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 
 const apiKey = '8592766d75eb4787b9609d395d45e5ce';
-
 const main  = document.querySelector('main');
 const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'bbc-news';
 
-window.addEventListener('load', e => {
+
+window.addEventListener('load', async e => {
     updateNews();
-    updateSources();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    //fetch news based on chance
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+
+    if('serviceWorker' in navigator){
+        try {
+            navigator.serviceWorker.register('sw.js');
+            console.log('sw registered');
+        }catch (error) {
+            console.log('sw failed');
+        }
+    }
 });
 
 async function updateSources(){
@@ -20,8 +37,8 @@ async function updateSources(){
 }
 
 
-async function updateNews(params) {
-    const res  = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`)
+async function updateNews(source = defaultSource) {
+    const res  = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`)
     const json = await res.json();
 
     main.innerHTML = json.articles.map(createArticle).join('\n');
